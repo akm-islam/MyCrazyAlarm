278eae2d811f686f0132bb0b06908eb1b471c330
Mamunjahir <Mamunjahir.com@gmail.com>
2018-01-15 23:51:00 -0500

    close icon Added

--- app/src/main/java/com/example/mamunjahir/clocktest/main/MainActivity.java
+++ app/src/main/java/com/example/mamunjahir/clocktest/main/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.mamunjahir.clocktest.main;
 
+import android.app.ActionBar;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -40,6 +43,10 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
         spinner = (Spinner) findViewById(R.id.spinner2);
         numberProblems = new NumberProblems();
+        Toolbar toolbar=findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_close_black_24dp);
         // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.country_arrays, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -58,7 +65,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
             alarmManager.set(AlarmManager.RTC_WAKEUP, 5000, pendingIntent);
         }
     }
-
+// This is for spinner
     @Override
     public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
         Toast.makeText(MainActivity.this, "Selected " + getResources().getStringArray(R.array.country_arrays)[i], Toast.LENGTH_SHORT).show();
@@ -69,4 +76,17 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     public void onNothingSelected(AdapterView<?> adapterView) {
 
     }
+// This is for ActionBar
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+    // Action bar ends
 }
